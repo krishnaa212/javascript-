82417cc325118665ffb82ff18b8f251da41edfb5
krishnaa212 <151374583+krishnaa212@users.noreply.github.com>
2024-10-06 16:17:27 +0000

    strings

---
+++ 01-basics/strings.js
@@ -0,0 +1,17 @@
+const  name="Krishna";
+const repoCount=2;
+
+//string interpolation --> ` ${} `
+// console.log(`Hello my name is ${name} and my repocount is ${repoCount}`);
+
+const gameName = new String("Krishnaaaa1");
+
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+
+const newName="Krishna";
+console.log(newName.slice(4,7));
+
+const newWord="krishna-didwania-hello-world";
+console.log(newWord.split("-"));
