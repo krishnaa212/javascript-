ab4cca7376bd3ef8a058451082758deb28ae6aa6
krishnaa212 <151374583+krishnaa212@users.noreply.github.com>
2024-10-06 17:31:55 +0000

    maths

--- 01_basics/5_nums&math.js
+++
@@ -1,5 +0,0 @@
-const score=400;
-console.log(score);
-
-const balance=new SVGAnimatedInteger(100);
-console.log(balance);
---
+++ 01_basics/5_nums_and_math.js
@@ -0,0 +1,17 @@
+const score=400;
+console.log(score);
+
+const balance=new Number(100);
+console.log(balance);
+
+const hundreds=1000000
+console.log(hundreds.toLocaleString());
+console.log(hundreds.toLocaleString('en-IN'));
+
+
+//MATHS
+
+console.log(Math.round(10.6329763));
+const max=20;
+const min=10;
+console.log(Math.floor((Math.random())*(max-min+1)+min));
