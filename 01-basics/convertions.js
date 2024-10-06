04fbfea4eac79d0449bd3fa0c484fa3e2f46f2bd
krishnaa212 <151374583+krishnaa212@users.noreply.github.com>
2024-10-06 11:33:58 +0000

    conversions

---
+++ 01-basics/convertions.js
@@ -0,0 +1,15 @@
+let score="33"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// console.log(null >= 0)
+// console.log(null == 0)
+
+// >= converts string to number but == doesnt
+
+//=== strict comparison
+// compares data type as well
+
+console.log("2"==2);
+console.log("2"===2);
