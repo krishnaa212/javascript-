ca36899077a9ee63d37aca20c0a3f6599bbdace3
krishnaa212 <151374583+krishnaa212@users.noreply.github.com>
2024-10-12 19:23:15 +0000

    dates

---
+++ 01_basics/6_dates.js
@@ -0,0 +1,20 @@
+//DATE is a object
+
+let myDate=new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+
+
+let myCreatedDate=new Date("2024-01-23");
+// console.log(myCreatedDate.toLocaleDateString());
+// console.log(myCreatedDate.toDateString());
+
+let timestamp= Date.now();
+console.log(timestamp);
+console.log(Math.floor((timestamp)/1000));
+
+
