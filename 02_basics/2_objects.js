20e1ef6dbe8bc5f58b65689b06d234282ed43aa8
krishnaa212 <151374583+krishnaa212@users.noreply.github.com>
2024-10-24 13:50:12 +0000

    objects 1

---
+++ 02_basics/2_objects.js
@@ -0,0 +1,26 @@
+// singleton
+
+const mySym= Symbol("key1");
+const JsUser = {
+    name : "Krishna", //behind the scene name is in string format  
+    age : 18,
+    "class roll" : 002211701087,
+    emailId : "krishnadidwania17@gmail.com",
+    loggedIn: false,
+    [mySym] : "mykey1"
+}
+// console.log(JsUser.name);
+// console.log(JsUser["age"]);//so the key should be in quotes
+// console.log(JsUser[mySym]);
+// console.log(mySym);
+
+// JsUser.age=19;
+// console.log(JsUser.age); //overriding an object using =
+
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log(`HELLO Js USER,${this.name}`);
+    
+}
+console.log(JsUser.greeting())
